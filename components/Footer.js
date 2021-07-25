fd4f4f82f572e9cd768a60353ef61f4414be59b2
Santosh Dawanse <dawanse.santosh@gmail.com>
2021-07-25 09:56:44 +0545

    footer added

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mastfoot mt-auto text-center">
+      <div className="inner">
+        <p>Next auth example, by <a href="https://thedawnsoft.blogspot.com">dawnsoft</a>.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
